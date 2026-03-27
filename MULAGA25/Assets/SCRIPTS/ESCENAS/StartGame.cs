c4242ddc94989286174da311d7042c81e71d66f9
sab1103b <sanbol2369@gmail.com>
2026-03-27 01:28:31 -0500

    interacciones

--- MULAGA25/Assets/SCRIPTS/ESCENAS/StartGame.cs
+++ MULAGA25/Assets/SCRIPTS/ESCENAS/StartGame.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
     public FadeController fade;
+    public string sceneToLoad;
 
-    public string sceneToLoad; 
+    public void SetLevel(string sceneName)
+    {
+        sceneToLoad = sceneName;
+        Debug.Log("Nivel seleccionado: " + sceneToLoad);
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("MainCamera"))
+        if (other.CompareTag("MainCamera") && !string.IsNullOrEmpty(sceneToLoad))
         {
             StartCoroutine(fade.FadeOut(sceneToLoad));
         }
